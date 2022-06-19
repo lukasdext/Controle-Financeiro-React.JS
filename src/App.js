de2bae7f742e18b56d4991f794a683f9492a1b9c
lukasdext <lukasdext160@hotmail.com>
2022-06-19 19:29:03 -0300

    subindo projeto/1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './components';
+import GlobalStyle from './styles/global';
+
+const App = () => {
+    return(
+    <>
+        <Header />
+        <GlobalStyle />
+    </>
+    );
+};
+
+export default App;
+
